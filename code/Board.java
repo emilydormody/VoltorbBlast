6407b69a0417f1cd5631c422eaf891ab0343ea56
emoly <emdormody@mun.ca>
2023-06-15 20:22:30 -0230

    yellows

--- code/Board.java
+++ code/Board.java
@@ -5,8 +5,11 @@ public class Board extends JFrame {
     private BoardSquare[][] board;
     public Board() {
         this.setSize(new Dimension(700,700));
-        this.setLayout(new GridLayout(6,6));
-        this.setBackground(new Color(41, 133, 66));
+        this.setLayout(new FlowLayout());
+        JPanel panel = new JPanel(new GridLayout(6,6));
+        panel.setBackground(Color.red);
+        panel.setPreferredSize(new Dimension(700,700));
+        //new Color(41, 133, 66) new Color(50, 168, 82)
         // 41, 133, 66
         board = new BoardSquare[6][6];
         for (int x=0; x<6; x++){
@@ -16,14 +19,20 @@ public class Board extends JFrame {
                     square = new BoardSquare(x,y, true);}
                 else{
                     square = new BoardSquare(x,y,false);}
-                square.setBackground(new Color(50, 168, 82));
+                square.setBackground(Color.orange);
+                square.setOpaque(true);
                 square.add(new JLabel(String.valueOf(x+y)));
-                square.setBorder(new RoundBorder());
-                getContentPane().add(square);
+                if (x+y%2 ==0 ){
+                    square.setBorder(BorderFactory.createLineBorder(Color.white, 5));}
+                else{
+                    square.setBorder(BorderFactory.createLineBorder(Color.black, 5));
+                }
+                panel.add(square);
                 board[x][y] = square;
                 }
 
             }
+        getContentPane().add(panel);
         setVisible(true);}
 
 
