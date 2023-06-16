05cbc296b10bba334416967e8636c51ec129e6ba
emoly <emdormody@mun.ca>
2023-06-16 12:59:17 -0230

    added side panel

--- code/Board.java
+++ code/Board.java
@@ -4,11 +4,14 @@ import java.awt.*;
 public class Board extends JFrame {
     private BoardSquare[][] board;
     public Board() {
-        this.setSize(new Dimension(700,700));
-        this.setLayout(new FlowLayout());
-        JPanel panel = new JPanel(new GridLayout(6,6, 7,7));
-        panel.setBackground(new Color(50, 168, 82));
-        panel.setPreferredSize(new Dimension(700,700));
+        this.setSize(new Dimension(1100,700));
+        this.setLayout(new BorderLayout());
+        JPanel boardPanel = new JPanel(new GridLayout(6,6, 7,7));
+        JPanel sidePanel = new JPanel(new GridLayout(2,1));
+        boardPanel.setPreferredSize(new Dimension(700,700));
+        boardPanel.setBackground(new Color(50, 168, 82));
+        sidePanel.setPreferredSize(new Dimension(400,700));
+        sidePanel.setBackground(Color.gray);
         //new Color(41, 133, 66) new Color(50, 168, 82)
         // 41, 133, 66
         board = new BoardSquare[6][6];
@@ -19,17 +22,14 @@ public class Board extends JFrame {
                     square = new BoardSquare(x,y, true);}
                 else{
                     square = new BoardSquare(x,y,false);}
-                square.setBackground(new Color(41, 133, 66));
-                square.setSize(25,25);
-                square.setOpaque(true);
-                square.add(new JLabel(String.valueOf(x+y)));
-                square.setBorder(BorderFactory.createLineBorder(Color.white, 5));
-                panel.add(square);
+
+                boardPanel.add(square);
                 board[x][y] = square;
                 }
 
             }
-        getContentPane().add(panel);
+        getContentPane().add(boardPanel, BorderLayout.CENTER);
+        getContentPane().add(sidePanel, BorderLayout.EAST);
         setVisible(true);}
 
 
--- code/BoardSquare.java
+++ code/BoardSquare.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class BoardSquare extends JButton {
 
     public BoardSquare(int x, int y, boolean edge){
+        this.setBackground(new Color(41, 133, 66));
+        this.setSize(25,25);
+        this.setOpaque(true);
+        this.setText(String.valueOf(x+y));
+        this.setBorder(BorderFactory.createLineBorder(Color.white, 5));
 
     }
 }
