55e5a60bcd6b84d6a60a014ac31d6a2382677389
emoly <emdormody@mun.ca>
2023-06-16 08:55:29 -0230

    gaps between buttons

--- code/Board.java
+++ code/Board.java
@@ -6,7 +6,7 @@ public class Board extends JFrame {
     public Board() {
         this.setSize(new Dimension(700,700));
         this.setLayout(new FlowLayout());
-        JPanel panel = new JPanel(new GridLayout(6,6));
+        JPanel panel = new JPanel(new GridLayout(6,6, 7,7));
         panel.setBackground(Color.red);
         panel.setPreferredSize(new Dimension(700,700));
         //new Color(41, 133, 66) new Color(50, 168, 82)
@@ -20,9 +20,10 @@ public class Board extends JFrame {
                 else{
                     square = new BoardSquare(x,y,false);}
                 square.setBackground(Color.orange);
+                square.setSize(25,25);
                 square.setOpaque(true);
                 square.add(new JLabel(String.valueOf(x+y)));
-                if (x+y%2 ==0 ){
+                if ((x+y)%2 ==0 ){
                     square.setBorder(BorderFactory.createLineBorder(Color.white, 5));}
                 else{
                     square.setBorder(BorderFactory.createLineBorder(Color.black, 5));
